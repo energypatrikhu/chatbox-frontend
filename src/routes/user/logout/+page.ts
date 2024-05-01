bfa98c1d6d1caee4c554fe903aa0a02eedbd4071
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-01 21:45:03 +0200

    Kijelentkezési oldal megvalósításának hozzáadása

---
+++ src/routes/user/logout/+page.ts
@@ -0,0 +1,19 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import api from '../../../lib/scripts/api';
+
+export const load = (async () => {
+	const loginId = localStorage.getItem('loginId');
+	if (!loginId) {
+		return redirect(302, '/user/login');
+	}
+
+	localStorage.removeItem('loginId');
+	try {
+		await api.delete(`/user/logout/${loginId}`);
+	} catch (error) {
+		console.error(error);
+	}
+
+	return redirect(302, '/user/login');
+}) satisfies PageLoad;
