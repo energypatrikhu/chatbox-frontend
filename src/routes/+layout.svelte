b24bfb0513c0f6eeed7a548eeb373d5bef81768a
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-01 21:52:05 +0200

    Alap Layout implementálása

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { userStore, userContactsStore } from '$lib/stores/user';
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<h1>ChatBox</h1>
+	<nav>
+		<a href="/">Főoldal</a>
+		<a href="/about">Rólunk</a>
+		<a href="/settings">Beállítások</a>
+	</nav>
+</header>
+
+{#if !$page.url.pathname.startsWith('/user')}
+	<aside>
+		{#each $userContactsStore as contact}
+			<div>
+				<span>{contact}</span>
+			</div>
+		{/each}
+	</aside>
+{/if}
+
+<main>
+	<slot />
+</main>
