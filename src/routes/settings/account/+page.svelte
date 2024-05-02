0c5f6a061c6a65b026fd098adfa00dbc5086271c
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 18:26:34 +0200

    Felhasználói adat módosítás oldal

---
+++ src/routes/settings/account/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { userStore } from '$lib/stores/user';
+</script>
+
+<h1>Account</h1>
+
+<label>
+	Email
+	<input
+		type="email"
+		bind:value="{$userStore.email}"
+	/>
+</label>
+
+<button type="submit">Mentés</button>
---
+++ src/routes/settings/profile/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { userStore } from '$lib/stores/user';
+</script>
+
+<h1>Profil</h1>
+
+<label>
+	Név
+	<input
+		type="text"
+		bind:value="{$userStore.name}"
+	/>
+</label>
+
+<button type="submit">Mentés</button>
