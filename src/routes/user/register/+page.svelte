17e672b3b288cb4503184be26a37f54bcda0bf1f
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-02 12:13:26 +0200

    Regisztráció implementálása

--- src/routes/user/register/+page.svelte
+++ src/routes/user/register/+page.svelte
@@ -1,1 +1,52 @@
-Register
+<script lang="ts">
+	import api from '$lib/scripts/api';
+
+	async function handleSubmit(event: Event) {
+		event.preventDefault();
+		const formData = new FormData(event.target as HTMLFormElement);
+		const data = Object.fromEntries(formData.entries());
+
+		try {
+			const registerResponse = (await api.put('user/register', data))
+				.data;
+
+			if (registerResponse.success) {
+				location.href = '/';
+			} else {
+				alert(registerResponse.message);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+</script>
+
+<h1>Regisztráció</h1>
+
+<form on:submit="{handleSubmit}">
+	<label>
+		Email
+		<input
+			type="email"
+			name="email"
+		/>
+	</label>
+
+	<label>
+		Név
+		<input
+			type="text"
+			name="name"
+		/>
+	</label>
+
+	<label>
+		Jelszó
+		<input
+			type="password"
+			name="password"
+		/>
+	</label>
+
+	<button type="submit">Regisztráció</button>
+</form>
