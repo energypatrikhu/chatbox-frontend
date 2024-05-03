a194449097db0b5a27a2bd5030ef97f19c7d9f6b
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-03 02:11:54 +0200

    Beállítások javítása

--- src/routes/settings/account/+page.svelte
+++ src/routes/settings/account/+page.svelte
@@ -4,14 +4,59 @@
 	let email = $userStore.email;
 </script>
 
-<h1>Account</h1>
+<div class="settings-container">
+	<div class="settings-header">
+		<h1>Account</h1>
+		<a href="/settings/profile">Profil</a>
+	</div>
 
-<label>
-	Email
-	<input
-		type="email"
-		bind:value="{$userStore.email}"
-	/>
-</label>
+	<div class="settings-body">
+		<div class="settings-body-setting">
+			<label class="setting-label">
+				E-Mail
+				<input
+					type="email"
+					bind:value="{email}"
+				/>
+			</label>
 
-<button type="submit">Mentés</button>
+			<button
+				class="setting-button"
+				on:click="{() => ($userStore.email = email)}">Mentés</button
+			>
+		</div>
+	</div>
+</div>
+
+<style>
+	.settings-container {
+		margin: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+	.settings-header {
+		display: flex;
+		flex-direction: column;
+		border-bottom: 1px solid #ccc;
+		padding-bottom: 10px;
+	}
+	.settings-body {
+		display: flex;
+		flex-direction: column;
+		width: 15rem;
+	}
+	.settings-body-setting {
+		display: flex;
+		flex-direction: column;
+	}
+	.setting-label {
+		display: flex;
+		flex-direction: column;
+	}
+	.setting-button {
+		width: 50%;
+		padding: 0.5rem;
+		margin-top: 1rem;
+	}
+</style>
--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -1,15 +1,62 @@
 <script lang="ts">
 	import { userStore } from '$lib/stores/user';
+
+	let name = $userStore.name;
 </script>
 
-<h1>Profil</h1>
+<div class="settings-container">
+	<div class="settings-header">
+		<h1>Profil</h1>
+		<a href="/settings/account">Account</a>
+	</div>
 
-<label>
-	Név
-	<input
-		type="text"
-		bind:value="{$userStore.name}"
-	/>
-</label>
+	<div class="settings-body">
+		<div class="settings-body-setting">
+			<label class="setting-label">
+				Név
+				<input
+					type="text"
+					bind:value="{name}"
+				/>
+			</label>
 
-<button type="submit">Mentés</button>
+			<button
+				class="setting-button"
+				on:click="{() => ($userStore.name = name)}">Mentés</button
+			>
+		</div>
+	</div>
+</div>
+
+<style>
+	.settings-container {
+		margin: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+	.settings-header {
+		display: flex;
+		flex-direction: column;
+		border-bottom: 1px solid #ccc;
+		padding-bottom: 10px;
+	}
+	.settings-body {
+		display: flex;
+		flex-direction: column;
+		width: 15rem;
+	}
+	.settings-body-setting {
+		display: flex;
+		flex-direction: column;
+	}
+	.setting-label {
+		display: flex;
+		flex-direction: column;
+	}
+	.setting-button {
+		width: 50%;
+		padding: 0.5rem;
+		margin-top: 1rem;
+	}
+</style>
