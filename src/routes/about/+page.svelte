1d2293ab07c903377ded9be2ec6f8e18c1f3a425
Joseph867 <145647037+Joseph867@users.noreply.github.com>
2024-05-03 14:19:48 +0200

    about ekezdése

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,9 @@
-About 
-
+<h1>Készítői: </h1>
+<div class="container">
+    <ul>
+        <li>Piovarcs Patrik</li>
+        <li>Schweizer Ádám</li>
+        <li>Gallai József</li>
+    </ul>
+    <p></p>
+</div>
