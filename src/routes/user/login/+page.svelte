d836a708324fef061de19b7dfa591b525e5cb7a4
Joseph867 <145647037+Joseph867@users.noreply.github.com>
2024-05-03 09:49:41 +0200

    login oldal css

--- src/routes/user/login/+page.svelte
+++ src/routes/user/login/+page.svelte
@@ -27,37 +27,63 @@
 	class="container"
 	style="width: 15rem;"
 >
-	<h1>Bejelentkezés</h1>
+	<div class="card card-3">
+		<div class="card-heading"></div>
+		<div class="card-body">
+			<h2 class="title">Bejelentkezés</h2>
+			<form on:submit="{handleSubmit}">
+				
+				<label class="w-50 p-2">
+					Email
+					<input
+						type="email"
+						name="email"
+						placeholder="Email cím"
+					/>
+				</label>
+				<label class="w-50 p-2">
+					Jelszó
+					<input
+						type="password"
+						name="password"
+						placeholder="Jelszó"
+					/>
+				</label>
+				<button
+					type="submit"
+					class="btn btn-info">Bejelentkezés</button
+				>
+			</form>
 
-	<form on:submit="{handleSubmit}">
-		<label class="w-50 p-2">
-			Email
-			<input
-				type="email"
-				name="email"
-			/>
-		</label>
-		<label class="w-50 p-2">
-			Jelszó
-			<input
-				type="password"
-				name="password"
-			/>
-		</label>
-		<button
-			type="submit"
-			class="btn btn-info">Bejelentkezés</button
-		>
-	</form>
-
-	<a
-		class="nav-link"
-		href="/user/register">Regisztráció</a
-	>
+			<a
+				class="nav-link"
+				href="/user/register">Regisztráció</a
+			>
+		
+		</div>
+	</div>
 </div>
 
 <style>
 	a {
 		color: black;
+	}
+	.card {
+		overflow: hidden;
+		-webkit-border-radius: 3px;
+		-moz-border-radius: 3px;
+		border-radius: 3px;
+		background: #fff;
+	}
+	.card-3 {
+		background: #000;
+		-webkit-border-radius: 10px;
+		-moz-border-radius: 10px;
+		border-radius: 10px;
+		-webkit-box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+		-moz-box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+		box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+		width: 100%;
+		display: table;
 	}
 </style>
