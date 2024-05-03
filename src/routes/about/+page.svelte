2ee393565bb0fb423d18a5c2dca7826c03e2b92b
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-03 14:33:59 +0200

    about szöveg javítás

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,19 +1,23 @@
 <h1>Készítői: </h1>
 <div class="container">
-    <ul>
-        <li>Piovarcs Patrik</li>
-        <li>Schweizer Ádám</li>
-        <li>Gallai József</li>
-    </ul>
-    <h3>chat mindenki számára</h3>
-    <p>Chat alkalmazás, ahol bárki bárkivel tud beszélgetni. Az alkalmazásban lehetőség van regisztrációra, bejelentkezésre, üzenetek küldésére, és azok megtekintésére. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is.</p>
-    <h3>További ötletek/fejlesztések:</h3>
-    <ul>
-        <li>emoji szekeztés</li>
-        <li>színes szöveg</li>
-        <li>szöveg formázás</li>
-        <li>saját gif</li>
-        <li>hangüzenet</li>
-        <li>videóüzenet</li>
-    </ul>
+	<ul>
+		<li>Piovarcs Patrik</li>
+		<li>Schweizer Ádám</li>
+		<li>Gallai József</li>
+	</ul>
+	<h3>chat mindenki számára</h3>
+	<p
+		>Chat alkalmazás, ahol bárki bárkivel tud beszélgetni. Az alkalmazásban
+		lehetőség van regisztrációra, bejelentkezésre, üzenetek küldésére, és
+		azok megtekintésére.</p
+	>
+	<h3>További ötletek/fejlesztések:</h3>
+	<ul>
+		<li>emoji szekeztés</li>
+		<li>színes szöveg</li>
+		<li>szöveg formázás</li>
+		<li>saját gif</li>
+		<li>hangüzenet</li>
+		<li>videóüzenet</li>
+	</ul>
 </div>
