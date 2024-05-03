5bccce3c4ae9be24904198f8a9ec43bc7d375dd7
Joseph867 <145647037+Joseph867@users.noreply.github.com>
2024-05-03 14:25:58 +0200

    leírás

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,5 +5,6 @@
         <li>Schweizer Ádám</li>
         <li>Gallai József</li>
     </ul>
-    <p></p>
+    <h3>chat mindenki számára</h3>
+    <p>Chat alkalmazás, ahol bárki bárkivel tud beszélgetni. Az alkalmazásban lehetőség van regisztrációra, bejelentkezésre, üzenetek küldésére, és azok megtekintésére. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is. Az alkalmazásban lehetőség van a felhasználók közötti privát üzenetváltásra is.</p>
 </div>
